035c2d29934c02a952b14dc9a1bb81f1d8e5bb2d
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-06-04 17:26:53 +0530

    I/O  command

---
+++ Cin and Cout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    int sum;
+    cout << "Enter the first number: ";
+    cin >> x;
+    cout << "Enter the second number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "x+y=" << sum;
+    return 0;
+}
