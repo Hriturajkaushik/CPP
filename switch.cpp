905d226677ff910b08c94ff4d52ed0a89d390cac
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-06-21 21:57:15 +0530

    Switch Statement

--- switch.cpp
+++ switch.cpp
@@ -48,5 +48,46 @@ int main()
 using namespace std;
 
 int main(){
-    
+
+    int num;       
+    cout<<"Enter a no. between 0-9 ";
+    cin>>num;
+
+   switch(num){
+
+    case 0:
+        cout<<"Zero";
+        break;
+    case 1:
+        cout<<"One";
+        break;
+    case 2:
+        cout<<"Two";
+        break;
+    case 3:
+        cout<<"Three";
+        break;
+    case 4:
+        cout<<"Four";
+        break;
+    case 5:
+        cout<<"Five";
+        break;
+    case 6:
+        cout<<"Six";
+        break;
+    case 7:
+        cout<<"Seven";
+        break;
+    case 8:
+        cout<<"Eight";
+        break;
+    case 9:
+        cout<<"Nine";
+        break;
+    default:
+        cout<<"Please enter a valid number between 0-9";
+        break;
+       }
+       return 0;
 }
