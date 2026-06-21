08ef959321ab4069b52cf8fd33844d1419ddadf3
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-06-21 21:11:12 +0530

    Switch Statement

---
+++ switch.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int day;
+    cout << "Enter the day: ";
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    default:
+        cout << "There are only 7 days in a week. Please enter a valid day.";
+        break;
+    }
+
+    return 0;
+}
+
+
+//---------------------EXAMPLE----------------------
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    
+}
