c674c48bb72ccbcc36cd75ac22eccce83f9dfb17
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-06-28 14:18:03 +0530

    Nested loop

--- Nested.cpp
+++ Nested.cpp
@@ -69,9 +69,11 @@ using namespace std;
 
 int main(){
 
-   for(int=0; i<=5; i++){
+   for(int=0; i<=5; i++)
+   {
     
-    for( int j = 0; j<=4; j++){
+    for( int j = 0; j<=4; j++)
+    {
       cout<<"*";
     }
    }
@@ -79,3 +81,24 @@ int main(){
 
    return 0;
 }
+
+
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    for( int i=1; i<=5; i++)
+    {
+        for( int j = 1; j<=i; j++)
+        {
+        cout<<"*";
+        }   
+        cout<<endl;
+    }
+    return 0;
+} 
+
