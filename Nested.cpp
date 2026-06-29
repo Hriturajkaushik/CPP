5911f7367a9623f4a3e2ccd8e1c174f3b932102a
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-06-29 14:37:48 +0530

    Nested Loop

--- Nested.cpp
+++ Nested.cpp
@@ -29,29 +29,6 @@ int main()
 
 
 #include <iostream>
-#include<string>
-#include<cmath>
-using namespace std;
-
-int main()
-{
-    // Outer loop runs 4 times (i = 1, 2, 3, 4)
-    for (int i = 1; i <= 4; i++)
-    {
-        cout << "My name is Hrituraj Kaushik " << i << "\n";
-
-        // Inner loop runs 5 times for EVERY outer loop turn
-        for (int j = 1; j <= 5; j++)
-        {
-            cout << "From section 2I" << "\n";
-        }
-    }
-    return 0;
-}
-
-
-
-#include <iostream>
 #include <string>
 #include <cmath>
 using namespace std;
