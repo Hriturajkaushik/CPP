e97de5488bd87dfcbdada54529c02d22881f5525
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-07-01 11:17:01 +0530

    Nested Loop

--- Nested.cpp
+++ Nested.cpp
@@ -129,7 +129,7 @@ int main()
     {
 
         for (int j = 1; j <= i; j++)
-            ;
+        
         {
             cout << j;
         }
@@ -184,7 +184,7 @@ int main()
     for (int i = 1; i <= 5; i++)
     {
 
-        for (int j = 0; j < 5; j++)
+        for (int j = 1; j <= 5; j++)
         {
 
             cout << i << " ";
